64528acea8b401011ab988534087d9b0d5098ec7
Stevie Ped <stephen.pederson.au@gmail.com>
2024-02-25 19:21:01 +1030

    Added makeRMRanges

---
+++ R/makeRMRanges.R
@@ -0,0 +1,142 @@
+#' @title Form a set of random, matching ranges for bootstrapping or permuting
+#'
+#' @description
+#' Form a set of ranges from y which (near) exactly match those in x for use as
+#' a background set requiring matching
+#'
+#' @details
+#' This function uses the width distribution of the 'test' ranges (i.e. `x`) to
+#' randomly sample a set of ranges with matching width from the ranges provided
+#' in `y`. The width distribution will clearly be exact when a set of
+#' fixed-width ranges is passed to `x`, whilst random sampling may yield some
+#' variability when matching ranges of variable width.
+#'
+#' When both x and y are GRanges objects, they are implcitly assumed to both
+#' represent similar ranges, such as those overlapping a promoter or enhancer.
+#' When passing two GRangesList objects, both objects are expected to contain
+#' ranges annotated as belonging to key features, such that the list elements in
+#' y must encompass all elements in x.
+#' For example if `x` contains two elements named 'promoter' and 'intron', `y`
+#' should also contain elements named 'promoter' and 'intron' and these will
+#' be sampled as matching ranges for the same element in `x`. If elements of
+#' `x` and `y` are not named, they are assumed to be in matching order.
+#'
+#' The default behaviour is to assume that randomly-generated ranges are for
+#' iteration, and as such, ranges are randomly formed in multiples of the number
+#' of 'test' ranges provided in `x`. The column `iteration` will be added to the
+#' returned ranges.
+#' Placing any number into the `n_total` argument will instead select a total
+#' number of ranges as specified here. In this case, no `iteration` column will
+#' be included in the returned ranges.
+#'
+#' Sampling is assumed to be with replacement as this is most suitable for
+#' bootstrapping and related procedures, although this can be disabled by
+#' setting `replace = FALSE`
+#'
+#' @return A GRanges or GRangesList object
+#'
+#'
+#' @param x GRanges with ranges to be matched
+#' @param y GRanges with ranges to select random matching ranges from
+#' @param exclude GRanges of ranges to omit from testing
+#' @param n_iter The number of times to repeat the random selection process
+#' @param n_total Setting this value will over-ride anything set using n_iter
+#' @param replace logical(1) Sample with our without replacement when creating
+#' the set of random ranges.
+#' @param mc.cores Passsed to \link[parallel]{mclapply}
+#' @param ... Not used
+#' @param force_ol logical(1) Enforce an overlap between every site in x and y
+#' @param unlist logical(1) Return as a sorted GRanges object, or leave as a
+#' GRangesList
+#'
+#' @rdname makeRMRanges-methods
+#' @aliases makeRMRanges
+#' @export
+setGeneric(
+  "makeRMRanges",
+  function(x, y, ...) standardGeneric("makeRMRanges")
+)
+#' @import GenomicRanges
+#' @importFrom IRanges overlapsAny width
+#' @importFrom GenomeInfoDb seqinfo
+#' @rdname makeRMRanges-methods
+#' @aliases makeRMRanges
+#' @export
+setMethod(
+  "makeRMRanges", signature = signature(x = "GRanges", y = "GRanges"),
+  function(
+    x, y, exclude = GRanges(), n_iter = 1, n_total = NULL, replace = TRUE, ...,
+    force_ol = TRUE
+  ) {
+
+    ## Check some basic features, e.g. y must be bigger than x
+    stopifnot(sum(width(y)) > sum(width(x)))
+    ## There must be overlap if bootstrapping.
+    if (force_ol) stopifnot(all(overlapsAny(x, y)))
+
+    # Make sure any new ranges don't fall off the end of chromosomes
+    stopifnot(is(exclude, "GRanges"))
+    sq_gr <- GRanges(seqinfo(x))
+    w <- width(x)
+    chr_ends <- c(
+      resize(sq_gr, width = max(w) / 2, fix = "start"),
+      resize(sq_gr, width = max(w) / 2, fix = "end")
+    )
+    exclude <- c(granges(exclude), chr_ends)
+    y <- setdiff(y, exclude)
+
+    ## Set the iterations
+    has_iter <- is.null(n_total)
+    if (has_iter) {
+      iter_size <- length(x)
+      n_total <- as.integer(n_iter * iter_size)
+    }
+
+    ## Now the random sampling
+    gpos <- GPos(y)
+    i <- sample(seq_along(gpos), n_total, replace = replace)
+    rand_w <- sample(w, n_total, replace = TRUE)
+    gr <- GRanges(gpos[i])
+    gr <- resize(gr, width = rand_w, fix = "center")
+    if (has_iter) gr$iteration <- rep(seq_len(n_iter), times = iter_size)
+    sort(gr)
+
+  }
+)
+#' @import GenomicRanges
+#' @importFrom parallel mclapply
+#' @rdname makeRMRanges-methods
+#' @aliases makeRMRanges
+#' @export
+setMethod(
+  "makeRMRanges",
+  signature = signature(x = "GRangesList", y = "GRangesList"),
+  function(
+    x, y, exclude = GRanges(), n_iter = 1, n_total = NULL, replace = TRUE,
+    mc.cores = 1, ..., force_ol = TRUE, unlist = TRUE
+  ){
+
+    stopifnot(length(x) <= length(y))
+    nm_x <- names(x)
+    nm_y <- names(y)
+    if (!is.null(nm_x) & !is.null(nm_y)) {
+      ## Set GRLists in matching order if named
+      stopifnot(all(nm_x %in% nm_y))
+      y <- y[nm_x]
+    }
+    i <- seq_along(x)
+    n_iter <- rep_len(n_iter, length(x))
+    n_total <- lapply(i, \(j) n_total[[j]])
+    out <- mclapply(
+      i,
+      \(i) makeRMRanges(
+        x[[i]], y[[i]], exclude = exclude,  n_iter = n_iter[[i]],
+        n_total = n_total[[i]], replace = replace, ..., force_ol = force_ol
+      ), mc.cores = mc.cores
+    )
+    if (!is.null(nm_x)) names(out) <- nm_x
+    out <- GRangesList(out)
+    if (unlist) out <- sort(unlist(out))
+    out
+  }
+)
---
+++ tests/testthat/test_makeRMRanges.R
@@ -0,0 +1,52 @@
+sq <- Seqinfo(seqnames = "chr1", seqlengths = 500, FALSE, "test")
+rng <- GRanges(c("chr1:101-150", "chr1:201-250"), seqinfo = sq)
+chr <- GRanges("chr1:51-500", seqinfo = sq)
+grl <- splitAsList(rng, c("promoter", "other"))
+y <- GRangesList(
+  promoter = GRanges("chr1:1-200"), other = GRanges("chr1:201-500")
+)
+seqinfo(y) <- sq
+
+test_that("Basic functions of makeRMRanges work", {
+  bg <- makeRMRanges(rng, chr, n_iter = 2)
+  expect_true(length(bg) == length(rng) * 2)
+  expect_true("iteration" %in% colnames(mcols(bg)))
+  expect_true(all(width(bg) == 50))
+
+  bg <- makeRMRanges(rng, chr, n_total = 3)
+  expect_true(length(bg) == 3)
+  expect_true(length(colnames(mcols(bg))) == 0)
+
+})
+
+test_that("GRangesList behaves as expected", {
+
+  bg <- makeRMRanges(grl, y, n_iter = 2)
+  expect_true("iteration" %in% colnames(mcols(bg)))
+  nm <- structure(
+    c(other = 2L, promoter = 2L), dim = 2L,
+    dimnames = structure(list(c("other", "promoter")), names = ""),
+    class = "table"
+  )
+  expect_equal(table(names(bg)), nm)
+
+  bg <- makeRMRanges(grl, y, n_total = c(2, 1))
+  nm <- structure(
+    c(other = 2L, promoter = 1L), dim = 2L,
+    dimnames = structure(list(c("other", "promoter")), names = ""),
+    class = "table"
+  )
+  expect_equal(table(names(bg)), nm)
+
+  bg <- makeRMRanges(grl, y, unlist = FALSE)
+  expect_equal(names(bg), c("other", "promoter"))
+  expect_true(all(vapply(bg, length, integer(1)) == 1))
+
+})
+
+test_that("makeRMRAnges errors as expected", {
+  expect_error(makeRMRanges(rng, chr, "chr1:1"))
+  expect_error(makeRMRanges(rng, GRanges("chr1:150-120"))) # BG too short
+  expect_error(makeRMRanges(rng, GRanges("chr1:1-200"))) # Range 2 excluded
+  expect_error(makeRMRanges(grl, y[1]))
+})
