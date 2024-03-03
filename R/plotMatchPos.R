cb3686688f8a62025a8f6f2235388502893ee82f
Stevie Ped <stephen.pederson.au@gmail.com>
2024-03-03 13:07:07 +1030

    Finished plotMatchPos

--- R/plotMatchPos.R
+++ R/plotMatchPos.R
@@ -3,19 +3,53 @@
 #' @description
 #' Plot the distribution of motif matches across sequences
 #'
+#' @details
+#' Multiple options are provided for showing the distribution of PWM matches
+#' within a set of sequences, using either the smoothed probability density,
+#' the probability CDF or as a heatmap.
+#' Distances can be shown as symmetrical around the centre or using absolute distances
+#' from the central position within the sequences.
+#'
+#' Heatmaps are only enabled for comparisons across multiple PWMs
+#'
+#' @return A ggplot2 object
+#'
+#'
 #' @param matches Output from \link{getPwmMatches}
 #' @param binwidth Width of bins to use when plotting
 #' @param abs logical(1) Plot absolute distances from centre
 #' @param type Plot match density, the CDF or a binned heatmap
+#' @param geom Type of geom to be used for line plots. Ignored for heatmaps
 #' @param ... Passed to individual geom* functions
 #'
+#' @examples
+#' ## Load the example PWM
+#' data("ex_pwm")
+#' esr1 <- ex_pwm$ESR1
+#'
+#' ## Load the example Peaks
+#' data("ar_er_peaks")
+#' library(BSgenome.Hsapiens.UCSC.hg19)
+#' genome <- BSgenome.Hsapiens.UCSC.hg19
+#' seq <- getSeq(genome, ar_er_peaks)
+#'
+#' ## Just the best match
+#' bm <- getPwmMatches(esr1, seq, best_only = TRUE)
+#' plotMatchPos(bm, se = FALSE)
+#'
+#' ## Matches can also be hjown by distance from centre
+#' plotMatchPos(bm, abs = TRUE)
+#'
+#' ## Cumulative Probability plots are also implemented
+#' plotMatchPos(bm, type = "cdf", geom = "line", colour = "red") +
+#'   geom_abline(intercept = 0.5, slope = 1/ 400)
 #'
 #' @importFrom S4Vectors splitAsList
 #' @import ggplot2
 #' @export
 plotMatchPos <- function(
     matches, binwidth = 10, abs = FALSE, type = c("density", "cdf", "heatmap"),
-    ...
+    geom = c("smooth", "line", "point", "col"), ...
 ){
   .checkMatches(matches)
   type <- match.arg(type)
@@ -55,16 +89,19 @@ plotMatchPos <- function(
   x <- sym("bin_centre")
   if (abs) x <- sym("bin_start")
 
+  geom <- paste0("geom_", match.arg(geom))
+  geom_fun <- match.fun(geom)
   if (type == "density") {
     p <- ggplot(df, aes({{ x }}, p, colour = {{ name }})) +
-      geom_smooth(se = FALSE, method = "loess", formula = 'y ~ x', ...)
+      geom_fun(...)
   }
   if (type == "cdf") {
     int <- ifelse(abs, 0, 0.5)
     p <- ggplot(df, aes({{ x }}, cdf, colour = {{ name }})) +
-      geom_line(...)
+      geom_fun(...)
   }
   if (type == "heatmap") {
+    if (is.null(name)) stop("Heatmaps not inplemented for a single PWM")
     p <- ggplot(df, aes(bin, name, fill = p)) +
       geom_tile(...) +
       scale_x_discrete(expand = rep_len(0, 4)) +
