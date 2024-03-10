fd8f6ecb18552e7ab9c7ea06eb0775965ef9a7c7
Stevie Ped <stephen.pederson.au@gmail.com>
2024-03-10 18:26:17 +1030

    Setup for pkgdown

---
+++ data-raw/sticker.R
@@ -0,0 +1,11 @@
+library(hexSticker)
+sticker(
+    subplot = "data-raw/image.png",
+    s_x = 0.4, s_y = 1.1,
+    s_width = 2, s_height = 2,
+    package = "motifTestR",
+    p_size = 26, p_color = 1, p_y = 1,
+    filename = "inst/figures/favicon.png",
+    h_fill = "#FEFEFE", h_color = "#000000",
+    white_around_sticker = TRUE
+)
