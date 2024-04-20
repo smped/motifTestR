42d405efb49c615344cd7b89b6af79445973f67c
Stevie Ped <stephen.pederson.au@gmail.com>
2024-04-20 12:36:34 +0930

    Added hypergeometric test

--- tests/testthat/test_testMotiEnrich.R
+++ tests/testthat/test_testMotiEnrich.R
@@ -40,17 +40,20 @@ test_that("Quasipoisson works", {
 
 })
 
-# test_that("NegBinom works", {
-#     mcols(bg_set) <- mcols(bg_ranges)
-#     iter <- suppressWarnings(
-#         # 10 is too few iterations in reality
-#         testMotifEnrich(esr1, test_set, bg_set, model = "neg")
-#     )
-#     expect_true(is(iter, "data.frame"))
-#     expect_true(nrow(iter) == 1)
-#     expect_true(iter$n_iter == 10)
-#
-# })
+test_that("Hypergeometric works", {
+    mcols(bg_set) <- mcols(bg_ranges)
+    hg <- testMotifEnrich(esr1, test_set, bg_set, model = "hyper")
+    expect_true(is(hg, "data.frame"))
+    expect_true(nrow(hg) == 1)
+    expect_true("odds_ratio" %in% colnames(hg))
+    expect_message(
+        testMotifEnrich(
+            esr1, test_set, c(bg_set, test_set[1]), model = "hyper"
+        ),
+        "Shared.+"
+    )
+
+})
 
 test_that("Errors are caught",{
     expect_error(.testIter(list(esr1), test_set, bg_set[-1]))
