e593b6ea4fae685efb7c7038de37845d7f34b503
Stevie Ped <stephen.pederson.au@gmail.com>
2024-10-16 11:43:04 +1030

    Cleaned up everything (hopefully)

--- inst/scripts/ex_pwm.R
+++
@@ -1,8 +0,0 @@
-#' Script used to create example motifs
-#' All taken from the coreA set of HOCOMOCOv11
-library(MotifDb)
-ex_pwm <- MotifDb |>
-  subset(organism == "Hsapiens") |>
-  query(andStrings = "HOCOMOCOv11-core-A", orStrings = c("ESR1", "ANDR", "FOXA1", "ZNF")) |>
-  as.list()
-names(ex_pwm) <- gsub(".+-core-A-([A-Z0-9]+)_HUMAN.+", "\\1", names(ex_pwm))
