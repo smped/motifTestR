7c893b79e22f35f7c2b4e18ee53a01b4132d0759
Stevie Ped <stephen.pederson.au@gmail.com>
2025-03-10 23:49:19 +1030

    Added Poisson & NB capabilities when simulating sequences

--- tests/testthat/test_simSeq.R
+++ tests/testthat/test_simSeq.R
@@ -14,6 +14,24 @@ test_that("Basic sequence simulation works", {
     expect_true(is(sim_seq, "character"))
 })
 
+test_that("Poisson simulation works", {
+    set.seed(1000)
+    sim_seq <- simSeq(10, 5, pfm, rate = 0.5)
+    ## There should always be at least one NA
+    expect_true(any(is.na(mcols(sim_seq)$pos)))
+    ## For a width of 5, can only be pos 1 or 2
+    expect_true(max(mcols(sim_seq)$pos, na.rm = TRUE) <= 2)
+})
+
+test_that("NegBin simulation works", {
+    set.seed(1000)
+    sim_seq <- simSeq(10, 5, pfm, rate = 1, theta = 1)
+    ## There should always be at least one NA
+    expect_true(any(is.na(mcols(sim_seq)$pos)))
+})
+
 test_that("simSeq errors where expected", {
     expect_error(simSeq(1, 3, pfm))
+    expect_error(simSeq(1, 4, pfm, rate = -1))
+    expect_error(simSeq(1, 4, pfm, rate = 1, theta = -1))
 })
